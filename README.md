b994735b5168829afb991bf5a6c4f4aac7b86623
Scott Sauyet <scott@sauyet.com>
2023-01-02 23:18:35 -0500

    Start this up

---
+++ README.md
@@ -0,0 +1,26 @@
+Tiddlywiki5 Nearby Plugin
+============================
+
+This adds previous/next links to the footer of certain groups of tiddlers.
+
+The main documentation is at https://crosseye.github.io/TW5-Nearby/.
+
+
+Installation
+------------
+
+For Node.js installations, copy the folder `plugins/Nearby` into your project's
+`plugins` folder and restart the server.
+
+For other installatiion, open https://crosseye.github.io/TW5-Nearby/, drag 
+the plugin link to your wiki, and choose "Install".
+
+
+
+Version History
+---------------
+
+You can see the [version history][vh] in the demo.
+
+
+  [vh]: https://crosseye.github.io/TW5-Nearby/#%24%3A%2Fplugins%2FScottSauyet%2FNearby%2Fversion
---
+++ plugins/Nearby/$__plugins_ScottSauyet_Nearby_neighbors.js
@@ -0,0 +1,67 @@
+/*\
+title: $:/core/modules/filters/neighbors.js
+type: application/javascript
+module-type: filteroperator
+
+List the `n` nearest neighbors for a tiddler
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+
+const intersect = (as, bs) => 
+  as .filter (a => bs .includes (a))
+
+
+const fields = (tiddler) => Object .fromEntries ( // TODO: Very roundabout.  Surely something better available
+  Object .entries (tiddler .getFieldStrings()) 
+  .filter (([k]) => ! ['created', 'modified', 'revision', 'tags', 'text', 'title', 'type'] .includes (k))
+  .map (([k, v]) => [k, tiddler.getFieldList(k)])
+)
+
+const points = {
+    tagged: 8,
+    linked: 5,
+    sharedTag: 3,
+    hasField: 1
+}
+
+const distance = function (wiki, first, second) {
+    const a = wiki.getTiddler (first) ?? {fields: {}}
+    const b = wiki.getTiddler (second) ?? {fields: {}}
+    const linked = wiki.getTiddlerLinks(first).includes(second) || wiki.getTiddlerLinks(second).includes (first)
+    const tagsA = a.getFieldList('tags')
+    const tagsB = b.getFieldList('tags')
+    const tagged = tagsA.includes (second) || tagsB.includes (first)
+    const sharedTag = intersect (tagsA, tagsB) . length > 0
+    const fieldsA = Object .values(fields (a)) .flat()
+    const fieldsB = Object .values(fields (b)) .flat ()
+    const hasField = fieldsA .includes (second) || fieldsB .includes (first)
+    const rawScore = 
+        (tagged ? points.tagged : 0) +
+        (linked ? points.linked : 0) +
+        (sharedTag ? points.sharedTag : 0) +
+        (hasField ? points.hasField : 0)
+    return first == second ? 0 : rawScore == 0 ? Infinity : ~~(1000 / rawScore)
+}
+
+exports.neighbors = function(source,operator,options) {
+	const titles = options.wiki.getTiddlers() .filter (t => ! t.startsWith ('$:/'))
+	let results = []
+	source(function(tiddler,title) { // todo: can this operate on more than one?  Here it's the last input tiddler
+		results = titles 
+		  .map (t => [t, distance(options.wiki, title, t)]) 
+		  .filter (([_, v]) => v !== 0 && v !== Infinity)
+		  .sort (([, a], [, b]) => a - b) 
+		  .slice (0, operator .operand ? Number (operator .operand) : Infinity)
+		  .map (([t]) => t
+		)
+	});
+	return results
+};
+
+})();
